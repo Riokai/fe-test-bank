22b4704cfdbfaa1de380dab1b2d4513a2dfb8a92
Kai <zengkai2009@foxmail.com>
2016-05-25 11:20:55 +0800

    add: title component

---
+++ src/components/Title.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="page-header">
+    <h1>
+      <slot></slot>
+    </h1>
+  </div>
+</template>
+
+<style scoped>
+  .page-header h1 {
+    font-weight: bold;
+  }
+</style>
