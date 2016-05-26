6aff94799725da751e1660e029461f4db03168be
Kai <zengkai2009@foxmail.com>
2016-05-26 20:19:53 +0800

    optimize: active status

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,14 +1,22 @@
 <template>
   <ul class="nav nav-list">
-    <li v-for="(name, item) in data" @click="toggleMenu(name)" style="cursor: pointer;">
-      <a v-link="isStr(item) ? { path: `/admin/${name}` } : {}">
+    <li
+      v-for="(name, item) in data"
+      @click="toggleMenu(name)"
+      style="cursor: pointer;"
+      :class="{'active': activeItem === name && isStr(item)}">
+      <a
+        v-link="isStr(item) ? { path: `/admin/${name}` } : {}"
+        @click="isStr(item) && (activeItem = name)">
 				<i class="icon-dashboard"></i>
 				<span class="menu-text">{{ isStr(item) ? item : item.title }}</span>
         <b v-show="!isStr(item)" class="arrow icon-angle-down"></b>
 			</a>
       <ul class="submenu" v-if="!isStr(item)" v-show="activeMenu === name">
-        <li v-for="(sub_name, sub_item) in item.subMenu">
-          <a v-link="{ path: `/admin/${sub_name}` }">
+        <li
+          :class="{'active': activeItem === sub_name}"
+          v-for="(sub_name, sub_item) in item.subMenu">
+          <a @click.stop="activeItem = sub_name" v-link="{ path: `/admin/${sub_name}` }">
 						<i class="icon-dashboard"></i>
 						{{ sub_item }}
 					</a>
@@ -45,11 +53,27 @@
       }
     },
     created () {
-      console.log(1)
+      const targetName = this.$route.name
+
+      for (let name in this.data) {
+        if (this.isStr(this.data[name])) {
+          if (this.data[name] === targetName) {
+            this.activeItem = name
+          }
+        } else {
+          for (let subName in this.data[name].subMenu) {
+            if (this.data[name].subMenu[subName] === targetName) {
+              this.activeItem = subName
+              this.activeMenu = name
+            }
+          }
+        }
+      }
     },
     data () {
       return {
-        activeMenu: ''
+        activeMenu: '',
+        activeItem: ''
       }
     }
   }
